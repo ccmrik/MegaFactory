23935a9e3852c0a79f2c428a5048837d97fdfbb0
Rik <rik@example.com>
2026-03-22 10:20:37 +1100

    v1.1.6 - Version bump

--- MegaFactoryPlugin.cs
+++ MegaFactoryPlugin.cs
@@ -15,7 +15,7 @@ namespace MegaFactory
     {
         public const string PluginGUID = "com.rik.megafactory";
         public const string PluginName = "Mega Factory";
-        public const string PluginVersion = "1.1.5";
+        public const string PluginVersion = "1.1.6";
 
         internal static ManualLogSource Log;
         private static Harmony _harmony;
